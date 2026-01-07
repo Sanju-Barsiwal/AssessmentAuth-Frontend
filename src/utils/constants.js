af3279e4cd811b46c388eab6e207fd8e5d28a06e
Sanju-Barsiwal <sanjeev.kumar@iiitg.ac.in>
2026-01-07 10:56:37 +0530

    Configure for Vercel deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+export const BASE_URL = 
+  location.hostname === "localhost" 
+    ? "http://localhost:7777" 
+    : "https://authbackend-zeta.vercel.app";
